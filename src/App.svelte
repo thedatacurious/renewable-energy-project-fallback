187567de6bcdcc4aac21c830a535057c1dda9610
Pere <peroc79@gmail.com>
2023-01-09 11:43:51 +0100

    t

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,13 @@
   import SmallMultiple from "./lib/SmallMultiple.svelte";
   import { sankeyData, ChinaAfricaInfo, regionalNested } from "./data.js";
 
+  import * as d3 from "d3";
+  import { onMount } from 'svelte'; 
+  import  {joinedData,continentsArray,beeswarm_data}  from './data.js';
+  import Force from "./lib/Force.svelte";
+  //import Beeswarm from "./Components/Charts/Beeswarm.svelte";
+  
+
   $: sankeyYear = 2003;
   $: filteredSankeyData = sankeyData[sankeyYear];
   $: loanAmount = ChinaAfricaInfo[sankeyYear] ?? 0;
@@ -17,6 +24,140 @@
 
   const fullExtents = calcExtents(flatten(regionalNested), extentGetters);
   let chart = "Investment";
+
+//Force and beeswarm
+  let width=650;
+  let height=200;
+  let margin= {left: 40, right: 20, top: 20, bottom: 10};
+
+  let year = 2013;
+  let param_force='renewablesConsPerCap';
+
+  let params_force=Object.keys(joinedData[0]).filter(d=>d.includes('renewables'))
+  //renewablesConsPerCap
+  let params_labels={
+    renewablesCons: 'Primary energy consumption from renewables',
+    renewablesConsChangePercPct: "Annual percentage change in energy consumption from renewables",
+    renewablesConsChangePercTwh: "Annual change in other renewable consumption, measured in terawatt-hours",
+    renewablesConsPerCap: "Per capita electricity consumption from renewables",
+    renewablesGenPerCap: "Per capita electricity generation from renewables",
+    renewablesShare: "Share of primary energy consumption that comes from renewables",
+    renewables_share_elec: "Share of electricity generation that comes from renewables"
+  }
+
+  $ : beeswarm_data_filtered=beeswarm_data.filter((d)=>d.year==sankeyYear && d[param_force]);
+
+  $ : filtered=joinedData.filter((d)=>d.year==sankeyYear && d[param_force]);
+
+  let this_legend;
+  let force_svg;
+
+  onMount(() => 
+  {
+    let selectedCont=[];
+   
+   
+    let colorScale = d3.scaleOrdinal()
+                  .domain(continentsArray)
+                  .range(d3.schemeCategory10);
+ 
+   let legend=d3.select('.legend')
+    .attr('transform', 'translate(' + (margin.left + margin.right + 60) + ',' + (margin.top - 20) + ')')
+    .selectAll('g')
+    .data(continentsArray)
+    .enter()
+    .append('g')
+    .on('mouseover',function(e,g_data)
+    {
+      //the g
+      console.warn(d3.select(this))
+      d3.selectAll('.legend g').filter((d)=>
+      {
+        return d!==g_data;
+      }).attr('opacity',.6)
+    // console.info(d3.select(force_svg).selectAll('circle'))
+      let circles=d3.select(force_svg).selectAll('circle');
+      circles.attr('fill-opacity',.6)
+      circles.each(function(d, i)
+        {
+
+        let data=JSON.parse(d3.select(this).attr('data'))
+        if (data.continent!==g_data)
+        d3.select(this).attr('fill-opacity',.2)
+        })
+    })
+    .on('mouseout',function()
+    {
+      d3.selectAll('.legend g').attr('opacity',1)
+      d3.select(force_svg).selectAll('circle').attr('fill-opacity',.6)
+    })
+  /*
+  .on('click',function(e,g_data)
+  {
+    console.log(selectedCont)
+    let pos=selectedCont.indexOf(g_data);
+    if (pos==-1)
+    {      
+      //items 
+      selectedCont.push(g_data)
+    }
+    else
+    {      
+       selectedCont.splice(pos, 1);
+    }
+
+    d3.selectAll('.legend g').each(function(d)
+    {
+      console.log(d)
+      if (selectedCont.indexOf(d)>-1)
+      d3.select(this).attr('opacity',.6)
+      else
+      d3.select(this).attr('opacity',1)
+    })
+
+
+  })
+  */
+
+  legend.append('circle')
+  .attr('fill', (d, i) => 
+  {
+    return colorScale(d)
+    })   
+  .attr('transform','translate(0,8)')  
+  .attr('r', 8)
+  .attr('fill-opacity',.6)
+  
+  
+  legend.append('text')
+    .attr('x', 18)
+    .attr('y', 10)
+    .attr('dy', '.15em')
+    .text((d, i) => d)
+    .style('text-anchor', 'start')
+    .style('font-size', 12);
+
+    let sum=0;
+    legend.each(function(d,i)
+    {
+      
+      let node_width=(d3.select(this).node().getBBox().width);
+      
+      if (i>0)
+      {
+      
+        d3.select(this).attr('transform','translate('+sum+', 5)')
+        sum+=node_width+15;
+      }
+      
+      else
+      {
+        
+        sum+=node_width+15;
+    d3.select(this).attr('transform','translate(8, 5)')
+      }
+    })
+})
 </script>
 
 <main>
@@ -47,6 +188,45 @@
 
   <p>[Beeswarm plots placeholder]</p>
 
+  <div class="select">
+    <div>
+      <select bind:value={param_force}>
+        {#each Object.entries(params_labels) as p}
+    
+            <option id={p[0]} value={p[0]}>{p[1]}</option> 
+        {/each}
+      </select>
+    </div>
+<!--
+    <div>
+      <select bind:value={year}>
+        {#each years as d }
+            <option id={d}>{d}</option> 
+        {/each}
+      </select>
+    </div>
+    -->
+  </div>
+  <label for="basic-range">Years {sankeyYear} <small>From 2000 on</small></label>
+<!--
+<Range
+ bind:my_data={filtered} 
+  on:change={(e) => (year=e.detail.value)}
+  id="basic-slider"
+  min={2010}
+  max={2021}
+  initialValue={year}
+/>
+-->
+
+
+    <svg class="legend" {width} height=50 xmlns:svg='https://www.w3.org/2000/svg' viewBox='0 0 {width} 50' bind:this={this_legend} />
+
+    <svg class="force" {width} {height} xmlns:svg='https://www.w3.org/2000/svg' bind:this={force_svg} viewBox='0 0 {width} {height}'>
+      <Force {params_labels} {width} {param_force} {continentsArray} {height} year={sankeyYear} bind:my_data={filtered}/> 
+    </svg>
+    
+    
   <p>
     We take a look at how much investment each region is receiving for renewable
     energy from 2000 to 2020. Despite the high investment amount in Sub-Saharan
@@ -151,4 +331,61 @@
     width: 100%;
     height: 800px;
   }
+
+  
+  
+
+.legend
+{
+  cursor:pointer;
+}
+ .select select 
+ {
+  padding: 5px;
+    font-size: unset;
+    font-family: unset;
+    max-width: 55%;
+    float: left;
+        margin-right: 10px;
+            margin-top: 10px;
+ }
+
+  :global(.tooltip)
+  {
+    font-size:.8rem;
+  }
+
+  :global(.tooltip span)
+  {
+    display:block;
+    
+  }
+  main {
+    padding: 1em;
+    padding-top: 5rem;
+    margin: 0 auto;
+  }
+
+  :global(circle:hover)
+ {
+  cursor:pointer;
+ }
+  :global(.beeswarm .axis text,.yAxis text)
+  {
+    fill:white;
+    font-size:.7rem;
+  }
+  :global(.highlighted)
+  
+    {
+      padding:2px;
+      background:#ff9800;
+      color:white;
+    }
+  :global(.force),:global(.beeswarm)
+   {
+    
+    margin-bottom:3rem;
+  }
+
 </style>
---
+++ src/lib/beeswarm.svelte
@@ -0,0 +1,197 @@
+<script>
+    import * as d3 from "d3";
+    
+    export let width;
+    export let height;
+    
+    import Axis from '../utils/Axis.svelte';
+    
+    export let year;
+    export let param_force;
+    export let my_beeswarmdata;
+    export let continentsArray;
+  
+    let selected_datapoint;
+    let pinYAxis; // declare pins
+    let verticalLine;
+    
+    let padding = 0.5;
+    let margin= {left: 90, right: 20, top: 20, bottom: 10};
+    //height - margin.bottom - margin.top
+    let nodes;
+    let all_nodes;
+    
+    $ : values=my_beeswarmdata;    
+    
+    $ : xScale =d3.scaleLinear()
+      .domain([0,d3.max(my_beeswarmdata,d=>d[param_force])])
+      .nice()
+      .range([margin.left, width - margin.right])
+  
+    
+    $ : radiusScale = d3.scaleSqrt()
+      .domain([d3.min(my_beeswarmdata,d=>d.gdpPerCap),d3.max(my_beeswarmdata,d=>d.gdpPerCap)])
+      //.nice()
+      .range([2, 8])  
+    
+    $ : yScale = d3.scaleBand()
+      .domain(continentsArray)
+      .range([(height + margin.bottom), margin.top])    
+   
+              
+     // call axis generators on the scale and pin the SVG pins.
+      $: if (pinYAxis) {
+        
+        d3.select(pinYAxis).call(d3.axisLeft(yScale))
+        //.ticks(10);
+        //.ticks(10);
+        // select(pinXAxis).call(axisBottom(x).ticks(width / 60));
+        //.ticks(width / 60)
+      }
+    
+      
+    $ :  colorScale = d3.scaleOrdinal()
+      .domain(continentsArray)
+      .range(d3.schemeTableau10)
+    
+    
+    let tooltip = d3.select('body')
+          .append('div')          
+          .attr('class', 'beeswarm-tooltip tooltip')
+          .style('position', 'absolute')
+          .style('z-index', '1')
+          .style('visibility', 'hidden')
+          .style('padding', '10px')
+          .style('background', 'rgba(0,0,0,0.6)')
+          .style('border-radius', '4px')
+          .style('color', '#fff');
+    
+    
+    function mouseover_fx(e,d) 
+        {
+          let all=d3.select(all_nodes).selectAll('circle');
+          console.log(all.selectAll('increm').size())
+          
+          all.attr('opacity',.5)
+  
+          let _this=d3.select(e.target);
+          _this.attr('opacity',1);
+  
+          let this_r=d3.select(e.target).attr('r')
+          _this
+          .classed('increm',true)
+          .transition()
+          .duration(500)
+          .attr('r',this_r*2)
+          .on('end',()=>
+          {
+            _this
+            .classed('increm',false)
+            .transition()
+          .duration(100)
+          
+          .attr('r',this_r)
+          })       
+          
+          all.attr('opacity',1);
+  
+          var _line=d3.select(verticalLine);
+          _line.attr("x1", _this.attr("cx"))
+          .attr("y1", _this.attr("cy"))
+          .attr("y2", (height - margin.bottom))
+          .attr("x2",  _this.attr("cx"))
+          .attr("opacity", 1);
+  
+          const tooltipWidth = tooltip.node().offsetWidth;
+          const tooltipHeight = tooltip.node().offsetHeight;
+          tooltip
+                                .style("left", e.pageX - tooltipWidth/2 +'px')
+                                .style("top", e.pageY-tooltipHeight - 10+'px')
+                                .style('visibility', 'visible')
+                                
+                                .html(`<b>Country</b>: ${d.country} <br/>
+                                <b>Country</b>: ${d.continent} <br/>
+                                <b>Year</b>: ${d.year} <br/>
+                                <b>GDP per cap</b>: ${d.gdp.toLocaleString('en-US', {maximumFractionDigits: 2})}<br/>
+                                <b>Renewable Consumption per cap</b>: ${d[param_force]}`)
+        }
+       
+    
+       let BeedisplayData = [];        
+       
+       function update() {        
+        BeedisplayData = my_beeswarmdata
+        return my_beeswarmdata
+      }
+    
+        console.log(my_beeswarmdata)
+        console.info(param_force)
+    
+        $:simulation = d3.forceSimulation(my_beeswarmdata)
+        .force('x', d3.forceX((d) => xScale(d[param_force])).strength(2))
+        .force('y', d3.forceY((d) => yScale(d.continent)).strength(0.3))
+        .force('collide', d3.forceCollide(d => radiusScale(d.gdpPerCap) + padding).strength(1))    
+        //.tick();
+      
+      $:{
+            console.log('simulating a tick')
+            simulation.tick();
+        }
+      $:simulation.on("tick", update)
+ 
+    </script>
+    
+    <rect fill="black" stroke-width="2" {width} {height} stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke="#000000"/> 
+    
+    <g
+        class="yAxis"
+        bind:this={pinYAxis}
+        transform="translate({margin.left},-45)"
+      />
+     <Axis {width} {height} {margin} scale={xScale} position='bottom' /> 
+    <!-- <Axis {width} {height} {margin} scale={yScale} position='left' />  -->
+           
+    <g class='circles' bind:this={all_nodes} stroke="2">
+    
+    {#each BeedisplayData as d,i}
+    
+    <circle bind:this={nodes}
+        r={radiusScale(d.gdpPerCap) }
+        fill={colorScale(d.continent)}
+        fill-opacity={1}
+        cx={d.x}
+        cy={d.y}
+        on:mouseout={(e)=>
+        {
+          d3.select(verticalLine).attr('opacity',0);
+        }}
+        on:mouseover={(e) => {selected_datapoint = d; mouseover_fx(e,d)}}
+        on:mousemove={(e)=>{ 
+  
+          
+          const tooltipWidth = tooltip.node().offsetWidth;
+          const tooltipHeight = tooltip.node().offsetHeight;
+          tooltip
+            .style("left", e.pageX - tooltipWidth/2 +'px')
+            .style("top", e.pageY-tooltipHeight - 10+'px')
+        }}
+        on:mouseout={(e)=>{ 
+          tooltip.style('visibility', 'hidden')
+        }
+      }
+    
+    
+    />
+    
+    {/each}
+    </g>
+  
+    <line bind:this={verticalLine} stroke="white"
+    />
+  
+ 
+    
+    
+    
+        
+    
