2b89c27b278f0b36958557bba49906e9ca69f8a7
Pere <peroc79@gmail.com>
2023-01-10 01:40:24 +0100

    Update Beeswarm.svelte

--- src/lib/Beeswarm.svelte
+++ src/lib/Beeswarm.svelte
@@ -1,6 +1,6 @@
 <script>
     import * as d3 from "d3";
-    
+    import {interpolateInferno} from "https://cdn.skypack.dev/d3-scale-chromatic@3";    
     export let width;
     export let height;
     
@@ -8,25 +8,37 @@
     
     export let year;
     export let param_force;
+    export let params_labels;
     export let my_beeswarmdata;
+    export let regionsArray;
     export let continentsArray;
+    export let forceY_param;
+    //='targetRegion';
+
+    let arr_force;
+    
+    if (forceY_param=='targetRegion')
+    arr_force=regionsArray;
+      else
+      arr_force=continentsArray;
   
     let selected_datapoint;
     let pinYAxis; // declare pins
+    let pinXAxis;
     let verticalLine;
     
     let padding = 0.5;
-    let margin= {left: 90, right: 20, top: 20, bottom: 10};
+    let margin= {left: 120, right: 20, top: 0, bottom: 50};
     //height - margin.bottom - margin.top
     let nodes;
     let all_nodes;
     
-    $ : values=my_beeswarmdata;    
-    
+    //$ : values=my_beeswarmdata; 
+  
     $ : xScale =d3.scaleLinear()
-      .domain([0,d3.max(my_beeswarmdata,d=>d[param_force])])
+      .domain([d3.min(my_beeswarmdata,d=>d[param_force]),d3.max(my_beeswarmdata,d=>d[param_force])])
       .nice()
-      .range([margin.left, width - margin.right])
+      .range([margin.left, (width - margin.right-margin.left)])
   
     
     $ : radiusScale = d3.scaleSqrt()
@@ -35,25 +47,33 @@
       .range([2, 8])  
     
     $ : yScale = d3.scaleBand()
-      .domain(continentsArray)
-      .range([(height + margin.bottom), margin.top])    
-   
+      .domain(arr_force)
+      .range([(height + margin.bottom), margin.bottom])    
+     // .range([height + margin.bottom -50, margin.top-50])
               
      // call axis generators on the scale and pin the SVG pins.
       $: if (pinYAxis) {
         
         d3.select(pinYAxis).call(d3.axisLeft(yScale))
-        //.ticks(10);
-        //.ticks(10);
-        // select(pinXAxis).call(axisBottom(x).ticks(width / 60));
-        //.ticks(width / 60)
+     
+      }
+      $: if (pinXAxis) {
+        
+        d3.select(pinXAxis).call(d3.axisBottom(xScale))
+ 
       }
     
+      //we need more than 10 or 20 colors
+      $ : colorScale = d3.scaleSequential().domain([0,arr_force.length])
+        .interpolator(interpolateInferno);
+
+
+  function find_arr (region)
+    {
       
-    $ :  colorScale = d3.scaleOrdinal()
-      .domain(continentsArray)
-      .range(d3.schemeTableau10)
-    
+     // console.log(region,regionsArray.indexOf(region),colorScale(regionsArray.indexOf(region)))
+      return colorScale(arr_force.indexOf(region))
+    }
     
     let tooltip = d3.select('body')
           .append('div')          
@@ -94,14 +114,16 @@
           })       
           
           all.attr('opacity',1);
-  
+  /*
           var _line=d3.select(verticalLine);
           _line.attr("x1", _this.attr("cx"))
           .attr("y1", _this.attr("cy"))
           .attr("y2", (height - margin.bottom))
           .attr("x2",  _this.attr("cx"))
           .attr("opacity", 1);
+          */
   
+          
           const tooltipWidth = tooltip.node().offsetWidth;
           const tooltipHeight = tooltip.node().offsetHeight;
           tooltip
@@ -109,16 +131,21 @@
                                 .style("top", e.pageY-tooltipHeight - 10+'px')
                                 .style('visibility', 'visible')
                                 
-                                .html(`<b>Country</b>: ${d.country} <br/>
+                                .html(`<b>Region</b>: ${d.targetRegion} <br/>
+                                <b>Country</b>: ${d.country} <br/>
                                 <b>Continent</b>: ${d.continent} <br/>
                                 <b>Year</b>: ${d.year} <br/>
                                 <b>GDP per cap</b>: ${d.gdp.toLocaleString('en-US', {maximumFractionDigits: 2})}<br/>
-                                <b>Renewable Consumption per cap</b>: ${d[param_force]}`)
+                                <b>% of primary energy renewables consumption</b>: ${d['renewablesShareCon']}<br/>
+                                <b>`+params_labels[param_force]+`</b>: ${d[param_force]}
+                                
+                                `)
         }
        
     
        let BeedisplayData = [];        
-       
+   
+
        function update() {        
         BeedisplayData = my_beeswarmdata
         return my_beeswarmdata
@@ -129,41 +156,46 @@
     
         $:simulation = d3.forceSimulation(my_beeswarmdata)
         .force('x', d3.forceX((d) => xScale(d[param_force])).strength(2))
-        .force('y', d3.forceY((d) => yScale(d.continent)).strength(0.3))
-        .force('collide', d3.forceCollide(d => radiusScale(d.gdpPerCap) + padding).strength(1))    
+        .force('y', d3.forceY((d) => yScale(d[forceY_param])).strength(.3))
+        .force('collide', d3.forceCollide(d => radiusScale(d.gdpPerCap) + padding).strength(.3))    
         //.tick();
       
-      $:{
-            console.log('simulating a tick')
-            simulation.tick();
-        }
-      $:simulation.on("tick", update)
- 
+        $: simulation.on("tick", update)
+      
     </script>
     
-    <rect fill="black" stroke-width="2" {width} {height} stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke="#000000"/> 
-    
+   
+  
     <g
         class="yAxis"
         bind:this={pinYAxis}
-        transform="translate({margin.left},-45)"
+        transform="translate({margin.left},-{margin.bottom})"
       />
-     <Axis {width} {height} {margin} scale={xScale} position='bottom' /> 
+
+      <g
+        class="xAxis"
+        bind:this={pinXAxis}
+        
+        transform="translate(0,{(height -margin.bottom/2)+8})"
+      />
+     <!-- <Axis {width} {height} {margin} scale={xScale} position='bottom' />  -->
     <!-- <Axis {width} {height} {margin} scale={yScale} position='left' />  -->
            
-    <g class='circles' bind:this={all_nodes} stroke="2">
+    <g class='circles' bind:this={all_nodes} transform="translate(0,-35)">
     
     {#each BeedisplayData as d,i}
     
     <circle bind:this={nodes}
         r={radiusScale(d.gdpPerCap) }
-        fill={colorScale(d.continent)}
+        stroke="black"
+        stroke-width=.5
+        fill={find_arr(d[forceY_param])}
         fill-opacity={1}
         cx={d.x}
         cy={d.y}
         on:mouseout={(e)=>
         {
-          d3.select(verticalLine).attr('opacity',0);
+         // d3.select(verticalLine).attr('opacity',0);
         }}
         on:mouseover={(e) => {selected_datapoint = d; mouseover_fx(e,d)}}
         on:mousemove={(e)=>{ 
@@ -186,8 +218,7 @@
     {/each}
     </g>
   
-    <line bind:this={verticalLine} stroke="white"
-    />
+    <!-- <line bind:this={verticalLine} stroke="grey"/> -->
   
  
     
